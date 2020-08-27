0b87d89fd8105d2844c886481ef230417a1b8244
Nina <67241789+ninsajala@users.noreply.github.com>
2020-08-27 19:29:25 +0200

    added dependencies

---
+++ src/footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Footer() {
+
+    return(
+        <div className="footer" >
+            <em /> 
+            <p>Open source code by <a href="https://github.com/ninsajala/react-weather-final" title="Github repository" target="_blank" rel="noopener noreferrer">Nina</a>  <br />
+            Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik" target="_blank" rel="noopener noreferrer">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></p>
+
+        </div>
+    )
+
+}
