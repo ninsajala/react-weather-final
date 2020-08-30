4221ef443ec6dd212e7689c7dae4ff77d2334f65
Nina <67241789+ninsajala@users.noreply.github.com>
2020-08-30 15:51:10 +0200

    Update weather.js

--- src/weather.js
+++ src/weather.js
@@ -5,7 +5,6 @@ import Formatdate from "./formatdate"
 
 
 export default function Currentweather(props){
-    let iconSrc = `./images/${props.icon}.png`
 
    return (
     <div className="weather" >
@@ -13,7 +12,7 @@ export default function Currentweather(props){
     <span className="country"> ({props.country})</span>
     </h1>
     <p>Last updated: <Formattime timestamp={props.currenttime}/> </p>
-    <img className="icon" src={(iconSrc)} alt={props.description} />
+    <img className="icon" src={`./images/${props.icon}.png`} alt={props.description} />
     <p><Formatday timestamp={props.currenttime} /><br />
     <Formatdate timestamp={props.currenttime} /></p>
     <h2>{props.temp}°{props.unit} | {props.description} </h2>
